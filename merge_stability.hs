f3166d5035387c6997f923ff4c30afbb6382a3e3
Matthew Delaney <matt.delaney2@gmail.com>
2016-09-11 19:35:20 -0400

    Make interactive

--- merge_stability.hs
+++ merge_stability.hs
@@ -2,6 +2,9 @@
     but with a different approach to splitting
     that results in a stable sort -}
 
+import Control.Monad
+import System.IO
+
 data Record = Record { key :: Int
                      , value :: String
                      } deriving (Show)
@@ -28,16 +31,39 @@ mergeSort splitFn xs = let (as,bs) = splitFn xs
 
 putIntListLn = mapM_ (putStrLn . show)
 
+askForPos :: IO Int
+askForPos = do
+  putStr "Position: "
+  hFlush stdout
+  pos <- getLine
+  return $ read pos
+
+askForName :: IO String
+askForName = do
+  putStr "Name: "
+  hFlush stdout
+  getLine
+
+stopAdding :: IO Bool
+stopAdding = do
+  putStr "Add another? (y/n): "
+  hFlush stdout
+  resp <- getLine
+  return $ resp == "n"
+
+getRecords :: [Record] -> IO [Record]
+getRecords recs = do
+  pos <- askForPos
+  name <- askForName
+  let newRec = Record pos name
+  cont <- stopAdding
+  case cont of
+    True -> return $ recs ++ [newRec]
+    False -> getRecords (recs ++ [newRec])
+
+
 main = do
-        let res :: [Record]
-            res = [Record 5 "Apple"
-                  ,Record 5 "Starfruit"
-                  ,Record 5 "Melon"
-                  ,Record 4 "Banana"
-                  ,Record 3 "Pear"
-                  ,Record 2 "Grape"
-                  ,Record 1 "Pomegranate"
-                  ]
+        res <- getRecords []
         putStrLn "Unsorted:"
         putIntListLn res
         putStrLn "\nUnstable:"
